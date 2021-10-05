f9dd4b18dfbb70a8f8ebb2b82cc24f3982d9b94b
Trish <trishajjohnson@gmail.com>
2021-10-05 14:21:24 -0500

    save() function for reservation class

--- models/reservation.js
+++ models/reservation.js
@@ -38,7 +38,26 @@ class Reservation {
 
     return results.rows.map(row => new Reservation(row));
   }
+
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id, num_guests, start_at, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+        [this.customerId, this.numGuests, this.startAt, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE reservations SET customer_id=$1, numGuests=$2, startAt=$3, notes=$4
+             WHERE id=$5`,
+        [this.customerId, this.numGuests, this.startAt, this.notes, this.id]
+      );
+    }
+  }
 }
+
 
 
 module.exports = Reservation;
